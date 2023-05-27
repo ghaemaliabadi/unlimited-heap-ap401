c358ea066c802422462ba48a409fc2d616a02a4b
ghaem <ghaemaliabadi@gmail.com>
2023-05-27 17:45:02 +0330

    chore: test buttons for select train or ... page

--- lib/screens/ProjectMainPage.dart
+++ lib/screens/ProjectMainPage.dart
@@ -14,9 +14,38 @@ class _ProjectMainPage extends State<ProjectMainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title, style: Theme.of(context).textTheme.headline2),
+        // title: Text(widget.title, style: Theme.of(context).textTheme.headline2),
+        // remove app bar background color
+        backgroundColor: Colors.transparent,
+        elevation: 0.1,
+        // decrease top bar height
+        toolbarHeight: 2.0,
+        // elevation: 2.0,
+        centerTitle: true,
+        titleSpacing: 0.0,
+        // add back icon without leading space
       ),
-      body: Container(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            // row for back icon
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                // move to right side
+                padding: const EdgeInsets.fromLTRB(0, 12, 12, 0),
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+                  // back to previous page
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10.0),
+        ],
       ),
     );
   }

201f29b7f2fefd38eb5fe63b309c2cc5786fdb82
ghaem <ghaemaliabadi@gmail.com>
2023-05-27 21:28:37 +0330

    chore: init and test on drop down menus

--- lib/screens/ProjectMainPage.dart
+++ lib/screens/ProjectMainPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class ProjectMainPage extends StatefulWidget {
   const ProjectMainPage({super.key});
@@ -8,6 +9,19 @@ class ProjectMainPage extends StatefulWidget {
   @override
   State<ProjectMainPage> createState() => _ProjectMainPage();
 }
+final List<String> items = [
+  'تهران',
+  'مشهد',
+  'اصفهان',
+  'شیراز',
+  'گرگان',
+  'کرمان',
+  'تبریز',
+  'بوشهر',
+];
+String? selectedValue;
+final _formKey = GlobalKey<FormState>();
+final TextEditingController textEditingController = TextEditingController();
 
 class _ProjectMainPage extends State<ProjectMainPage> {
   late PageController _pageController = PageController();
@@ -16,8 +30,13 @@ class _ProjectMainPage extends State<ProjectMainPage> {
   @override
   void dispose() {
     _pageController.dispose();
+    textEditingController.dispose();
     super.dispose();
   }
+  final List<String> genderItems = [
+    'Male',
+    'Female',
+  ];
 
   @override
   void initState() {
@@ -31,10 +50,7 @@ class _ProjectMainPage extends State<ProjectMainPage> {
     //     .of(context)
     //     .size
     //     .width;
-    var pageHeight = MediaQuery
-        .of(context)
-        .size
-        .height;
+    var pageHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         // title: Text(widget.title, style: Theme.of(context).textTheme.headline2),
@@ -67,69 +83,209 @@ class _ProjectMainPage extends State<ProjectMainPage> {
             ],
           ), // Row For Back Icon
           SingleChildScrollView(
-            physics: const AlwaysScrollableScrollPhysics(),
-            child: GestureDetector(
-              onTap: () {
-                FocusScope.of(context).requestFocus(FocusNode());
-              },
-              child: Container(
-                // width: pageWidth,
-                height: pageHeight * 0.85,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(top: 0.0),
-                      child: _menuBar(context),
+            // scroll view for page view vertical scroll
+            physics: const NeverScrollableScrollPhysics(),
+            child: Container(
+              // width: pageWidth,
+              height: pageHeight * 0.85,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 0.0),
+                    child: _menuBar(context),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: PageView(
+                      controller: _pageController,
+                      // scroll  view for page view horizontal scroll
+                      physics: const AlwaysScrollableScrollPhysics(),
+                      onPageChanged: (int i) {
+                        FocusScope.of(context).requestFocus(FocusNode());
+                        setState(() {
+                          if (activePageIndex != i) {
+                            activePageIndex = i;
+                          }
+                        });
+                      },
+                      children: <Widget>[
+                        ConstrainedBox(
+                          constraints: const BoxConstraints.expand(),
+                          child: Form(
+                            key: _formKey,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 80),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  DropdownButtonFormField2(
+                                    decoration: InputDecoration(
+                                      //Add isDense true and zero Padding.
+                                      //Add Horizontal padding using buttonPadding and Vertical padding by increasing buttonHeight instead of add Padding here so that The whole TextField Button become clickable, and also the dropdown menu open under The whole TextField Button.
+                                      isDense: true,
+                                      contentPadding: EdgeInsets.zero,
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                      //Add more decoration as you want here
+                                      //Add label If you want but add hint outside the decoration to be aligned in the button perfectly.
+                                    ),
+                                    isExpanded: true,
+                                    hint: Text(
+                                      'مبدا رو انتخاب کنید',
+                                      style: Theme.of(context).textTheme.bodyText1,
+                                    ),
+                                    items: items
+                                        .map((item) => DropdownMenuItem<String>(
+                                      value: item,
+                                      child: Text(
+                                        item,
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                    ))
+                                        .toList(),
+                                    validator: (value) {
+                                      if (value == null) {
+                                        return 'مبدا';
+                                      }
+                                      return null;
+                                    },
+                                    onChanged: (value) {
+                                      //Do something when changing the item if you want.
+                                    },
+                                    onSaved: (value) {
+                                      selectedValue = value.toString();
+                                    },
+                                    buttonStyleData: const ButtonStyleData(
+                                      height: 60,
+                                      padding: EdgeInsets.only(left: 20, right: 10),
+                                    ),
+                                    iconStyleData: const IconStyleData(
+                                      icon: Icon(
+                                        Icons.arrow_drop_down,
+                                        color: Colors.black45,
+                                      ),
+                                      iconSize: 30,
+                                    ),
+                                    dropdownStyleData: DropdownStyleData(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                      maxHeight: 200,
+                                        scrollbarTheme: ScrollbarThemeData(
+                                          radius: const Radius.circular(40),
+                                          thickness: MaterialStateProperty.all(6),
+                                          thumbVisibility: MaterialStateProperty.all(true),
+                                        )
+                                    ),
+                                  ),
+                                  const SizedBox(height: 30),
+                                  DropdownButtonFormField2(
+                                    decoration: InputDecoration(
+                                      //Add isDense true and zero Padding.
+                                      //Add Horizontal padding using buttonPadding and Vertical padding by increasing buttonHeight instead of add Padding here so that The whole TextField Button become clickable, and also the dropdown menu open under The whole TextField Button.
+                                      isDense: true,
+                                      contentPadding: EdgeInsets.zero,
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                      //Add more decoration as you want here
+                                      //Add label If you want but add hint outside the decoration to be aligned in the button perfectly.
+                                    ),
+                                    isExpanded: true,
+                                    hint: Text(
+                                      'مقصد رو انتخاب کنید',
+                                      style: Theme.of(context).textTheme.bodyText1,
+                                    ),
+                                    items: items
+                                        .map((item) => DropdownMenuItem<String>(
+                                      value: item,
+                                      child: Text(
+                                        item,
+                                        style: Theme.of(context).textTheme.bodyText1,
+                                      ),
+                                    ))
+                                        .toList(),
+                                    validator: (value) {
+                                      if (value == null) {
+                                        return 'لطفا مقصد رو انتخاب کنید';
+                                      }
+                                      return null;
+                                    },
+                                    onChanged: (value) {
+                                      //Do something when changing the item if you want.
+                                    },
+                                    onSaved: (value) {
+                                      selectedValue = value.toString();
+                                    },
+                                    buttonStyleData: const ButtonStyleData(
+                                      height: 60,
+                                      padding: EdgeInsets.only(left: 20, right: 10),
+                                    ),
+                                    iconStyleData: const IconStyleData(
+                                      icon: Icon(
+                                        Icons.arrow_drop_down,
+                                        color: Colors.black45,
+                                      ),
+                                      iconSize: 30,
+                                    ),
+                                    dropdownStyleData: DropdownStyleData(
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(15),
+                                        ),
+                                        maxHeight: 200,
+                                        scrollbarTheme: ScrollbarThemeData(
+                                          radius: const Radius.circular(40),
+                                          thickness: MaterialStateProperty.all(6),
+                                          thumbVisibility: MaterialStateProperty.all(true),
+                                        )
+                                    ),
+                                  ),
+                                  const SizedBox(height: 30),
+                                  TextButton(
+                                    onPressed: () {
+                                      if (_formKey.currentState!.validate()) {
+                                        _formKey.currentState!.save();
+                                      }
+                                    },
+                                    child: const Text('Submit Button'),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        ConstrainedBox(
+                          constraints: const BoxConstraints.expand(),
+                          child: const Center(
+                            child: Text("تست ۲"),
+                          ),
+                        ),
+                        ConstrainedBox(
+                          constraints: const BoxConstraints.expand(),
+                          child: const Center(
+                            child: Text("تست ۳"),
+                          ),
+                        ),
+                        ConstrainedBox(
+                          constraints: const BoxConstraints.expand(),
+                          child: const Center(
+                            child: Text("تست ۴"),
+                          ),
+                        ),
+                      ],
                     ),
-                    Expanded(
-                      flex: 2,
-                      child: PageView(
-                        controller: _pageController,
-                        physics: const ClampingScrollPhysics(),
-                        onPageChanged: (int i) {
-                          FocusScope.of(context).requestFocus(FocusNode());
-                          setState(() {
-                            if (activePageIndex != i) {
-                              activePageIndex = i;
-                            }
-                          });
-                        },
-                        children: <Widget>[
-                          ConstrainedBox(
-                            constraints: const BoxConstraints.expand(),
-                            child: const Center(
-                              child: Text("تست"),
-                            ),
-                          ),
-                          ConstrainedBox(
-                            constraints: const BoxConstraints.expand(),
-                            child: const Center(
-                              child: Text("تست ۲"),
-                            ),
-                          ),
-                          ConstrainedBox(
-                            constraints: const BoxConstraints.expand(),
-                            child: const Center(
-                              child: Text("تست ۳"),
-                            ),
-                          ),
-                          ConstrainedBox(
-                            constraints: const BoxConstraints.expand(),
-                            child: const Center(
-                              child: Text("تست ۴"),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
+            // ),
           )
         ],
       ),
@@ -137,10 +293,7 @@ class _ProjectMainPage extends State<ProjectMainPage> {
   }
 
   Widget _menuBar(BuildContext context) {
-    var pageWidth = MediaQuery
-        .of(context)
-        .size
-        .width;
+    var pageWidth = MediaQuery.of(context).size.width;
     // var pageHeight = MediaQuery
     //     .of(context)
     //     .size
@@ -170,29 +323,26 @@ class _ProjectMainPage extends State<ProjectMainPage> {
         borderRadius: const BorderRadius.all(Radius.circular(1000)),
         onTap: onTap,
         child: Container(
-          width: MediaQuery
-              .of(context)
-              .size
-              .width,
+          width: MediaQuery.of(context).size.width,
           padding: const EdgeInsets.symmetric(vertical: 15),
           alignment: Alignment.center,
           decoration: (activePageIndex == index)
               ? BoxDecoration(
-            color: Theme.of(context).colorScheme.primary,
-            borderRadius: const BorderRadius.all(Radius.circular(1000)),
-          )
+                  color: Theme.of(context).colorScheme.primary,
+                  borderRadius: const BorderRadius.all(Radius.circular(1000)),
+                )
               : null,
           child: Text(
             name,
             style: (activePageIndex == index)
                 ? const TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold)
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold)
                 : const TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
           ),
         ),
       ),

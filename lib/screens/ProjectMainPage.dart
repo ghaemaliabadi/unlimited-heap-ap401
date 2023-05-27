abfc1fb35eeebcd3eb05da676e41f58b80a9d89b
ghaem <ghaemaliabadi@gmail.com>
2023-05-27 18:01:43 +0330

    chore: add menubar and test it on main page

--- lib/screens/ProjectMainPage.dart
+++ lib/screens/ProjectMainPage.dart
@@ -10,8 +10,25 @@ class ProjectMainPage extends StatefulWidget {
 }
 
 class _ProjectMainPage extends State<ProjectMainPage> {
+  late PageController _pageController;
+  int activePageIndex = 0;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
   @override
   Widget build(BuildContext context) {
+    var pageWidth = MediaQuery.of(context).size.width;
+    var pageHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         // title: Text(widget.title, style: Theme.of(context).textTheme.headline2),
@@ -20,7 +37,6 @@ class _ProjectMainPage extends State<ProjectMainPage> {
         elevation: 0.1,
         // decrease top bar height
         toolbarHeight: 2.0,
-        // elevation: 2.0,
         centerTitle: true,
         titleSpacing: 0.0,
         // add back icon without leading space
@@ -44,9 +60,141 @@ class _ProjectMainPage extends State<ProjectMainPage> {
               ),
             ],
           ),
-          const SizedBox(height: 10.0),
+          SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
+            child: GestureDetector(
+              onTap: () {
+                FocusScope.of(context).requestFocus(FocusNode());
+              },
+              child: Container(
+                width: pageWidth,
+                height: pageHeight * 0.85,
+                decoration: const BoxDecoration(
+                  color: Color(0XFFE0E0E0),
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 0.0),
+                      child: _menuBar(context),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: PageView(
+                        controller: _pageController,
+                        physics: const ClampingScrollPhysics(),
+                        onPageChanged: (int i) {
+                          FocusScope.of(context).requestFocus(FocusNode());
+                          setState(() {
+                            activePageIndex = i;
+                          });
+                        },
+                        children: <Widget>[
+                          ConstrainedBox(
+                            constraints: const BoxConstraints.expand(),
+                            child: Center(
+                              child: Text("تست"),
+                            ),
+                          ),
+                          ConstrainedBox(
+                            constraints: const BoxConstraints.expand(),
+                            child: Center(
+                              child: Text("تست ۲"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
+  }
+  Widget _menuBar(BuildContext context) {
+    return Container(
+      width: 300.0,
+      height: 50.0,
+      decoration: const BoxDecoration(
+        color: Color(0XFFE0E0E0),
+        borderRadius: BorderRadius.all(Radius.circular(1000)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: InkWell(
+              borderRadius: const BorderRadius.all(Radius.circular(1000)),
+              onTap: _onPlaceBidButtonPress,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                alignment: Alignment.center,
+                decoration: (activePageIndex == 0)
+                    ? const BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.all(Radius.circular(1000)),
+                      )
+                    : null,
+                child: Text(
+                  "پرواز داخلی",
+                  style: (activePageIndex == 0)
+                      ? const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        )
+                      : const TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: InkWell(
+              borderRadius: const BorderRadius.all(Radius.circular(1000)),
+              onTap: _onBuyNowButtonPress,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                alignment: Alignment.center,
+                decoration: (activePageIndex == 1)
+                    ? const BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.all(Radius.circular(1000)),
+                      )
+                    : null,
+                child: Text(
+                  "پرواز خارجی",
+                  style: (activePageIndex == 1)
+                      ? const TextStyle(
+                          color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)
+                      : const TextStyle(
+                          color: Colors.black, fontSize: 20,  fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onPlaceBidButtonPress() {
+    _pageController.animateToPage(0,
+        duration: const Duration(milliseconds: 500), curve: Curves.decelerate);
+  }
+
+  void _onBuyNowButtonPress() {
+    _pageController.animateToPage(1,
+        duration: const Duration(milliseconds: 500), curve: Curves.decelerate);
   }
 }

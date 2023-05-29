cf8766725896aba19314c657d852ee0e45f2ea33
mohadeseh <hakimi.d.f.25@gmail.com>
2023-05-29 23:59:07 +0330

    chore: login page responds to touch

--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -17,72 +17,77 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       appBar: AppBar(
         elevation: 1.5,
-        // Theme.of(context).textTheme.headline2 is a TextStyle object that
-        // defined in lib/main.dart with white color
         title: Text(widget.title, style: Theme.of(context).textTheme.headline2),
       ),
-      // add two input for enter username and password
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const SizedBox(height: 200.0),Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 16),
-              child: Text(
-                  "Test LoginPage",
-                  style: Theme.of(context).textTheme.headline1
-              )
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-            child: TextField(
-              decoration: InputDecoration(
-                alignLabelWithHint: true,
-                labelText: 'نام کاربری',
-                // hintText: 'نام کاربری خود را وارد کنید',
-                border: OutlineInputBorder(),
-                suffixIcon: Icon(Icons.person),
-              ),
+      body: InkResponse(
+        containedInkWell: false,
+        highlightColor: Colors.transparent,
+        splashColor: Colors.transparent,
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 200.0),Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 16),
+                child: Text(
+                    "Test LoginPage",
+                    style: Theme.of(context).textTheme.headline1
+                )
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'رمز عبور',
-                // hintText: 'رمز عبور',
-                border: OutlineInputBorder(),
-                suffixIcon: Icon(Icons.lock),
-              ),
-            ),
-          ),
-          // submit button
-          const SizedBox(height: 16.0),
-          ElevatedButton(
-            onPressed: () => {
-              _showSnackBar(context, 'ورود با موفقیت انجام شد.'),
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        const SignUpPage()),
-              ),
-            },
-            style: ButtonStyle(
-              minimumSize: MaterialStateProperty.all(const Size(150.0, 50.0)),
-              shape: MaterialStateProperty.all(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(1000.0),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  alignLabelWithHint: true,
+                  labelText: 'نام کاربری',
+                  // hintText: 'نام کاربری خود را وارد کنید',
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(Icons.person),
                 ),
               ),
             ),
-            child: Text(
-              'ورود',
-              style: (Theme.of(context).textTheme.headline2),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'رمز عبور',
+                  // hintText: 'رمز عبور',
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(Icons.lock),
+                ),
+              ),
             ),
-          ),
-        ],
+            // submit button
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () => {
+                _showSnackBar(context, 'ورود با موفقیت انجام شد.'),
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          const SignUpPage()),
+                ),
+              },
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all(const Size(150.0, 50.0)),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(1000.0),
+                  ),
+                ),
+              ),
+              child: Text(
+                'ورود',
+                style: (Theme.of(context).textTheme.headline2),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

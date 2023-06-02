02d1913f37f695ad3b55ee9d7ddac03a41dd3690
mohadeseh <hakimi.d.f.25@gmail.com>
2023-06-02 20:56:01 +0330

    chore: added sample username and divider to account page

--- lib/screens/AccountPage.dart
+++ lib/screens/AccountPage.dart
@@ -11,6 +11,7 @@ class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: DefaultTabController(
         length: 3,
         child: Scaffold(
@@ -48,28 +49,36 @@ class _AccountPageState extends State<AccountPage> {
               ),
             )
           ),
-          body: const TabBarView(
+          body: TabBarView(
             children: [
-              Center(
+              const Center(
                 child: Text('تراکنش‌ها'),
               ),
-              Center(
+              const Center(
                 child: Text('سفرها'),
               ),
               Padding(
-                  padding: EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.all(16.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Card(
-                        margin: EdgeInsets.symmetric(vertical: 20.0),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10.0),
                         color: Colors.transparent,
-                        elevation: 0.0,
-                        child: CircleAvatar(
-                          radius: 50.0,
+                        child: const CircleAvatar(
+                          radius: 40.0,
                           backgroundColor: Colors.transparent,
                           backgroundImage: AssetImage('assets/images/user.png'),
                         ),
+                      ),
+                      Text(
+                        'نام کاربری',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                        ),
+                      Divider(
+                        height: 20.0,
+                        color: Theme.of(context).colorScheme.primary,
+                        thickness: 1.5,
                       ),
                     ],
                   ),

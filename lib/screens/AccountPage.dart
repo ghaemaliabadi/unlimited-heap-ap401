50a928380e33e3f55c4ffcd7b6baf0ed5fbdcc37
mohadeseh <hakimi.d.f.25@gmail.com>
2023-06-02 17:10:41 +0330

    chore: app bar for account page

--- lib/screens/AccountPage.dart
+++ lib/screens/AccountPage.dart
@@ -10,6 +10,48 @@ class AccountPage extends StatefulWidget {
 class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(80.0),
+            child: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              elevation: 1.5,
+              bottom: TabBar(
+                tabs: [
+                  Tab(
+                      icon: const Icon(Icons.credit_card),
+                      child: Text(
+                        'تراکنش‌ها',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                  ),
+                  Tab(
+                      icon: const Icon(Icons.luggage),
+                      child: Text(
+                        'سفرها',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                  ),
+                  Tab(
+                      icon: const Icon(Icons.person),
+                      child: Text(
+                        'حساب کاربری',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                  ),
+                ],
+              ),
+            )
+          ),
+          body: const TabBarView(
+            children: [
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

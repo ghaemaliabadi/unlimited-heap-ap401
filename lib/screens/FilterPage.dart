f8f151bd7c9a353f7e496841ccba5bd18dedcd9f
ghaem <ghaemaliabadi@gmail.com>
2023-06-05 02:51:16 +0330

    chore: add range slider for time

--- lib/screens/FilterPage.dart
+++ lib/screens/FilterPage.dart
@@ -12,24 +12,53 @@ class FilterPage extends StatefulWidget {
 }
 
 class _FilterPageState extends State<FilterPage> {
-
+  var _startValue = 4.5;
+  var _endValue = 23.5;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // resizeToAvoidBottomInset: false,
       appBar: AppBar(
         elevation: 1.5,
-        title: Text('test',
+        title: Text('فیلترها',
             style: Theme.of(context).textTheme.displayMedium),
       ),
-      body: Column (
-        children: [
-          // print companies
-          Text(widget.companies.toString()),
-          // print tags
-          Text(widget.tags.toString()),
-        ],
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column (
+          children: [
+            // range slider from 04:30 to 23:30
+            RangeSlider(
+              values: RangeValues(_startValue, _endValue),
+              min: 4.5,
+              max: 23.5,
+              divisions: 19,
+              labels: RangeLabels(
+                  convertIntToTimeStr(_startValue.toDouble()),
+                  convertIntToTimeStr(_endValue.toDouble())
+              ),
+              onChanged: (RangeValues values) {
+                setState(() {
+                  _startValue = values.start;
+                  _endValue = values.end;
+                });
+              },
+            ),
+            // print companies
+            Text(widget.companies.toString()),
+            // print tags
+            Text(widget.tags.toString()),
+          ],
+        ),
       )
     );
   }
+}
+
+String convertIntToTimeStr(double time) {
+  var intPart = time ~/ 1;
+  var decimalPart = time - intPart;
+  var hour = intPart;
+  var minute = (decimalPart * 60).round();
+  return '${hour < 10 ? '0' : ''}$hour:${minute < 10 ? '0' : ''}$minute';
 }

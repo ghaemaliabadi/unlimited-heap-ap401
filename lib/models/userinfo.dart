ba260a3127f89f7dc573bad6058d74d986ae142c
mohadeseh <hakimi.d.f.25@gmail.com>
2023-06-08 22:51:12 +0330

    chore: deleted duplicate code

--- lib/models/userinfo.dart
+++ lib/models/userinfo.dart
@@ -1,4 +1,5 @@
 import 'package:persian_datetime_picker/persian_datetime_picker.dart';
+import 'tripsTaken.dart';
 
 class User {
   User({
@@ -50,18 +51,4 @@ class User {
   void setEmail(String text) {
     email = text;
   }
-}
-
-convertEnToFa(String txt) {
-  return txt
-      .replaceAll('0', '۰')
-      .replaceAll('1', '۱')
-      .replaceAll('2', '۲')
-      .replaceAll('3', '۳')
-      .replaceAll('4', '۴')
-      .replaceAll('5', '۵')
-      .replaceAll('6', '۶')
-      .replaceAll('7', '۷')
-      .replaceAll('8', '۸')
-      .replaceAll('9', '۹');
 }

66faad547eed7fed2371294f251bb4ddc468986e
ghaem <ghaemaliabadi@gmail.com>
2023-06-09 17:55:15 +0330

    chore: added return ticket and passengers details

--- lib/screens/submitDetailsPage.dart
+++ lib/screens/submitDetailsPage.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:unlimited_heap_ap401/models/passenger.dart';
 
 import '../models/trip.dart';
 
 // ignore: must_be_immutable
 class SubmitDetailsPage extends StatefulWidget {
   final Trip tripData;
+
   const SubmitDetailsPage({super.key, required this.tripData});
 
   @override
@@ -21,108 +23,190 @@ class SubmitDetailsPageState extends State<SubmitDetailsPage> {
             style: Theme.of(context).textTheme.displayMedium),
       ),
       body: SingleChildScrollView(
-        physics: const AlwaysScrollableScrollPhysics(),
-        child: Column(
-          children: [
-            buildTicketDetails(widget.tripData!.departTicket),
-            () {
-             if (widget.tripData.type == 'رفت و برگشت') {
-               // return buildTicketDetails();
-               return const SizedBox();
-             } else {
-               return const SizedBox();
-             }
-            } (),
-          ],
-        )
-    ),
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: Column(
+            children: [
+              buildTicketDetails(widget.tripData.departTicket, 'بلیط رفت'),
+              () {
+                if (widget.tripData.type == 'رفت و برگشت') {
+                  return buildTicketDetails(
+                      widget.tripData.returnTicket, 'بلیط برگشت');
+                } else {
+                  return const SizedBox();
+                }
+              }(),
+              buildPassengersDetails(widget.tripData.passengerList),
+              const SizedBox(height: 64),
+            ],
+          )),
     );
   }
 
-  Padding buildTicketDetails(ticket) {
+  Padding buildTicketDetails(ticket, ticketTitle) {
     return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.surface,
-                borderRadius: BorderRadius.circular(8),
-                border: Border.all(
-                  color: Theme.of(context).colorScheme.secondary,
-                  width: 2.5,
+      padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: Theme.of(context).colorScheme.secondary,
+            width: 2.5,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                const Icon(
+                  Icons.sticky_note_2_outlined,
+                  color: Colors.black87,
+                  size: 32,
                 ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.sticky_note_2_outlined,
+                const SizedBox(width: 8),
+                Text(
+                  ticketTitle,
+                  style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                        fontSize: 24,
                         color: Colors.black87,
-                        size: 32,
                       ),
-                      const SizedBox(width: 8),
-                      Text(
-                        'بلیط رفت',
-                        style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                              fontSize: 24,
-                              color: Colors.black87,
-                            ),
-                      ),
-                    ],
-                  ),
-                  const Divider(),
-                  RowGeneratorForTicket('مبدا', ticket.from),
-                  const Divider(),
-                  RowGeneratorForTicket('مقصد', ticket.to),
-                  const Divider(),
-                  RowGeneratorForTicket('تاریخ و ساعت', ticket.dateString + '  -  ' + ticket.inboundTimeString),
-                  const Divider(),
-                  RowGeneratorForTicket('شرکت', ticket!.company.name),
-                  const Divider(),
-                  RowGeneratorForTicket('نوع بلیط', ticket.transportBy),
-                  const Divider(),
-                  RowGeneratorForTicket('قیمت بلیط', '${ticket!.priceString} تومان'),
-                  const SizedBox(height: 16),
-                ],
-              ),
+                ),
+              ],
             ),
-          );
-  }
-
-  Row RowGeneratorForTicket(title, value) {
-    return Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    title,
-                    style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                      fontSize: 20,
-                          color: Colors.black87,
-                    ),
-                  ),
-                  const Spacer(),
-                  Text(
-                    value,
-                    style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                      color: Colors.black54,
-                    ),
-                  ),
-                ],
-              );
-  }
-}
-
-void _showSnackBar(BuildContext context, String message) {
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: Text(
-        message,
-        style: Theme.of(context).textTheme.displaySmall,
+            const SizedBox(height: 16),
+            rowGeneratorForTicket('مبدا', ticket.from),
+            const Divider(),
+            rowGeneratorForTicket('مقصد', ticket.to),
+            const Divider(),
+            rowGeneratorForTicket('تاریخ و ساعت',
+                ticket.dateString + '  -  ' + ticket.inboundTimeString),
+            const Divider(),
+            rowGeneratorForTicket('شرکت', ticket!.company.name),
+            const Divider(),
+            rowGeneratorForTicket('نوع بلیط', ticket.transportBy),
+            const Divider(),
+            rowGeneratorForTicket('قیمت بلیط', '${ticket!.priceString} تومان'),
+            const SizedBox(height: 16),
+          ],
+        ),
       ),
-      duration: const Duration(seconds: 1),
-      backgroundColor: Theme.of(context).colorScheme.secondary,
-    ),
-  );
+    );
+  }
+
+  Row rowGeneratorForTicket(title, value) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          title,
+          style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                fontSize: 20,
+                color: Colors.black87,
+              ),
+        ),
+        const Spacer(),
+        Text(
+          value,
+          style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                color: Colors.black54,
+              ),
+        ),
+      ],
+    );
+  }
+
+  buildPassengersDetails(List<Passenger> passengerList) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: Theme.of(context).colorScheme.secondary,
+            width: 2.5,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                const Icon(
+                  Icons.person,
+                  color: Colors.black87,
+                  size: 32,
+                ),
+                const SizedBox(width: 8),
+                Text(
+                  'مسافران',
+                  style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                        fontSize: 24,
+                        color: Colors.black87,
+                      ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: passengerList.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    rowGeneratorForPassenger(
+                        'نام', passengerList[index].firstName),
+                    const Divider(),
+                    rowGeneratorForPassenger(
+                        'نام خانوادگی', passengerList[index].lastName),
+                    const Divider(),
+                    rowGeneratorForPassenger('کد ملی', passengerList[index].id),
+                    const Divider(),
+                    rowGeneratorForPassenger(
+                        'جنسیت', passengerList[index].gender),
+                    () {
+                     if (index != passengerList.length - 1) {
+                        return const Divider(
+                          height: 48,
+                          thickness: 2,
+                        );
+                      } else {
+                        return const SizedBox();
+                      }
+                    } (),
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row rowGeneratorForPassenger(String title, String? value) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          title,
+          style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                fontSize: 20,
+                color: Colors.black87,
+              ),
+        ),
+        const Spacer(),
+        Text(
+          value!,
+          style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                color: Colors.black54,
+              ),
+        ),
+      ],
+    );
+  }
 }

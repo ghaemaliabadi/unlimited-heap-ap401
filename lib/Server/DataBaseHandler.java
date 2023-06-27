8438eaabf7b811b7c2cd1e50242446d720fa48c0
mohadeseh <hakimi.d.f.25@gmail.com>
2023-06-28 00:53:20 +0330

    chore: added a database handler to implement frequently used methods

---
+++ lib/Server/DataBaseHandler.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DataBaseHandler {
+    final File file;
+    DataBaseHandler(String fileName) {
+        this.file = new File(fileName);
+    }
+    String[] findUserRows(String username) {
+        ArrayList<String> rows = new ArrayList<>();
+        try {
+            Scanner reader = new Scanner(file);
+            String thisRow;
+            while (reader.hasNextLine()) {
+                thisRow = reader.nextLine();
+                if (thisRow.split("-")[0].equals(username)) {
+                    System.out.println("im here");
+                    rows.add(thisRow);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("File not found!");
+        }
+        String[] out = new String[rows.size()];
+        return rows.toArray(out);
+    }
+}

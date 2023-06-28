f0f759321b98b2b5e85167503e7026936e3e163d
mohadeseh <hakimi.d.f.25@gmail.com>
2023-06-28 20:22:51 +0330

    chore: modified some of the fields on edit user info page

--- lib/screens/EditUserInfoPage.dart
+++ lib/screens/EditUserInfoPage.dart
@@ -8,10 +8,12 @@ class EditUserInfoPage extends StatefulWidget {
 }
 
 class _EditUserInfoPageState extends State<EditUserInfoPage> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     var pageHeight = MediaQuery.of(context).size.height;
+    var pageWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -26,80 +28,125 @@ class _EditUserInfoPageState extends State<EditUserInfoPage> {
         onTap: () {
           FocusScope.of(context).requestFocus(FocusNode());
         },
-        child: Container(
-          margin: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 35.0),
-          child: Column(
-            children: [
-              TextFormField(
-                style: Theme.of(context).textTheme.displaySmall,
+        child: Form(
+          key: _formKey,
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 35.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'لطفا نام خود را وارد کنید.';
+                    }
+                    return null;
+                  },
+                  style: Theme.of(context).textTheme.displaySmall,
+                    decoration: InputDecoration(
+                      alignLabelWithHint: true,
+                      labelText: 'نام',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                ),
+                SizedBox(height: pageHeight * 0.03,),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'لطفا نام خانوادگی خود را وارد کنید.';
+                    }
+                    return null;
+                  },
+                  style: Theme.of(context).textTheme.displaySmall,
                   decoration: InputDecoration(
                     alignLabelWithHint: true,
-                    labelText: 'نام',
+                    labelText: 'نام خانوادگی',
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15),
                     ),
                   ),
-              ),
-              SizedBox(height: pageHeight * 0.03,),
-              TextFormField(
-                style: Theme.of(context).textTheme.displaySmall,
-                decoration: InputDecoration(
-                  alignLabelWithHint: true,
-                  labelText: 'نام خانوادگی',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
                 ),
-              ),
-              SizedBox(height: pageHeight * 0.03,),
-              // form for birthdate
-              TextFormField(
-                style: Theme.of(context).textTheme.displaySmall,
-                decoration: InputDecoration(
-                  alignLabelWithHint: true,
-                  labelText: 'کد ملی',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-              ),
-              SizedBox(height: pageHeight * 0.03,),
-              TextFormField(
-                style: Theme.of(context).textTheme.displaySmall,
-                decoration: InputDecoration(
-                  alignLabelWithHint: true,
-                  labelText: 'شماره تماس',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    style: ButtonStyle(
-                      minimumSize:
-                      MaterialStateProperty.all(const Size(170.0, 50.0)),
-                      shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(1000.0),
+                SizedBox(height: pageHeight * 0.03,),
+                // form for birthdate
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: pageWidth * 0.4,
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'لطفا کد ملی خود را وارد کنید.';
+                          }
+                          return null;
+                        },
+                        style: Theme.of(context).textTheme.displaySmall,
+                        decoration: InputDecoration(
+                          alignLabelWithHint: true,
+                          labelText: 'کد ملی',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
                         ),
                       ),
                     ),
-                    child: Text('تایید اطلاعات',
-                        style: Theme.of(context).textTheme.displayMedium),
+                    SizedBox(width: pageWidth * 0.05,),
+                    SizedBox(
+                      width: pageWidth * 0.4,
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'لطفا شماره تماس خود را وارد کنید.';
+                          }
+                          return null;
+                        },
+                        style: Theme.of(context).textTheme.displaySmall,
+                        decoration: InputDecoration(
+                          alignLabelWithHint: true,
+                          labelText: 'شماره تماس',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('در حال ارسال اطلاعات...')),
+                          );
+                          Navigator.of(context).pop();
+                        }
+                      },
+                      style: ButtonStyle(
+                        minimumSize:
+                        MaterialStateProperty.all(const Size(170.0, 50.0)),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(1000.0),
+                          ),
+                        ),
+                      ),
+                      child: Text('تایید اطلاعات',
+                          style: Theme.of(context).textTheme.displayMedium),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-            ]
+                const SizedBox(
+                  height: 20.0,
+                ),
+              ]
+            ),
           ),
         ),
       )
